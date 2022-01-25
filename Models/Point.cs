f501c54fcc9891a4d4a42a8b36abe3e2c1df576b
Sujith-Sk-work <sujith.kumar@experionglobal.com>
2022-01-25 12:58:58 +0530

    Added award,awardHistory and point class

---
+++ Models/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace xcart.Models
+{
+    public class Point
+    {
+        [Column("Id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public long Id { get; set; }
+
+        [ForeignKey("UserId")]
+       // public virtual User UserId { get; set; }
+
+        [Column("TotalPoints")]
+        public long TotalPoints { get; set; }
+
+        [Column("CurrentPoints")]
+        public long CurrentPoints { get; set; }
+    }
+}
