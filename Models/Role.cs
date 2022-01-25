b653dc135686f9a71c5693c36e24c0e6db717c28
HaizonCruz <haizon.cruz@experionglobal.com>
2022-01-25 11:36:37 +0530

    Added IntialCreate

---
+++ Models/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace xcart.Models
+{
+    public class Role
+    {
+        [Column("Id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
