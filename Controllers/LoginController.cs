bd687f1c18646d145b724e186c52f399ac813027
HaizonCruz <haizon.cruz@experionglobal.com>
2022-01-27 15:53:23 +0530

    .

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -22,7 +22,7 @@ namespace xcart.Controllers
         }
 
         //Token Generation POST Method
-        [AllowAnonymous]
+        //[AllowAnonymous]
         [HttpPost("{userName}/{password}")]
         public async Task<IActionResult> Login(string userName, string password)
         {
@@ -44,7 +44,7 @@ namespace xcart.Controllers
             return response;
         }
 
-        [Authorize(Roles ="User")]
+        //[Authorize(Roles ="User")]
         //[Authorize(AuthenticationSchemes = "Bearer")]
         [HttpGet("{userName}")]
 
