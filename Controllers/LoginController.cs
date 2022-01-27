cae5dad28cb8e689f8a42f1b040e22f1c3dc3623
HaizonCruz <haizon.cruz@experionglobal.com>
2022-01-27 10:55:48 +0530

    Added Token

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using xcart.Services;
+
+namespace xcart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        ILoginService login;
+
+        public LoginController(ILoginService _login)
+        {
+            login = _login;
+        }
+
+        [HttpPost("{userName}/{password}")]
+        public IActionResult Login(string userName, string password)
+        {
+            IActionResult response = Unauthorized();
+
+            var user = login.ValidateUser(userName, password);
+
+            if(user!=null)
+            {
+                var tokenString = login.GenerateJWTToken(user);
+                response = Ok(new
+                {
+                    token = tokenString,
+                    userName = userName
+                }
+                    );
+                  
+                   
+  
+            }
+            return response;
+
+        }
+    }
+}
---
+++ Services/ILoginService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using xcart.Models;
+
+namespace xcart.Services
+{
+    public interface ILoginService
+    {
+        public User ValidateUser(string UserName, string password);
+
+        public string GenerateJWTToken(User user);
+
+    }
+}
