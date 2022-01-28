499a1734d6a5668984ce5db969b9e4583db394d9
Sujith-Sk-work <sujith.kumar@experionglobal.com>
2022-01-28 18:33:02 +0530

    .

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using xcart.Models;
@@ -24,7 +25,7 @@ namespace xcart.Controllers
         }
 
         [HttpGet]
-
+        [Authorize(Roles ="Admin")]
         public async Task<IActionResult> GetOrders()
         {
             var orders = await orderService.GetAllOrders();
