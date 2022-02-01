2e541b26652f15fb272745873427004741efc08f
abrahamannie <annie.abraham@experionglobal.com>
2022-02-01 13:31:45 +0530

    Item and Point Service

---
+++ Services/PointService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using xcart.Models;
+using xcart.ViewModel;
+
+namespace xcart.Services
+{
+    public class PointService : IPointService
+    {
+        XCartDbContext db;
+
+        public PointService(XCartDbContext db)
+        {
+            this.db = db;
+        }
+
+        #region Get Points By Element Id
+        public async Task<PointViewModel> GetPointsByEmployeeId(int id)
+        {
+            if (db != null)
+            {
+                //join User and Point
+                return await (from user in db.User
+                              from point in db.Point
+                              where user.Id == id && user.Id == point.User.Id
+                              select new PointViewModel
+                              {
+                                  Id = user.Id,
+                                  CurentPoints = point.CurrentPoints
+                              }).FirstOrDefaultAsync();
+            }
+            return null;
+        }
+        #endregion
+
+    }
+}
---
+++ ViewModel/PointViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace xcart.ViewModel
+{
+    public class PointViewModel
+    {
+        public int Id { get; set; }
+        public long CurentPoints { get; set; }
+
+    }
+}
