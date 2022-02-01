32c75178c3f8d59e53bd85da49d469f0c46aebbd
abrahamannie <annie.abraham@experionglobal.com>
2022-02-01 15:28:37 +0530

    employee change

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using xcart.Models;
+
+namespace xcart.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        XCartDbContext db;
+
+        public EmployeeService(XCartDbContext db)
+        {
+            this.db = db;
+        }
+        public async Task<Point> MostRewarded()
+        {
+            if (db != null)
+            { /*
+                return await (from point in db.Point
+                          orderby point.TotalPoints descending
+                          select point.User.Id).FirstOrDefaultAsync();
+                */
+            }
+            return null;
+        }
+    }
+}
