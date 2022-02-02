8528e180d83a269ea7302542ec12217ea4099d66
ashikahzain <ashika.hussain@experionglobal.com>
2022-02-02 10:52:15 +0530

    items list api

--- src/app/employee/home/home.component.ts
+++ src/app/employee/home/home.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/services/employee.service'
-import {SidemenuComponent} from 'src/app/shared/layout/sidemenu/sidemenu.component'
+import { SidemenuComponent } from 'src/app/shared/layout/sidemenu/sidemenu.component'
+import { DomSanitizer } from '@angular/platform-browser';
+import { Item } from 'src/app/shared/models/item';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
- 
-  constructor(public employeeservice: EmployeeService,public sidemenu:SidemenuComponent) { }
+  itemList: Item;
+  constructor(public employeeservice: EmployeeService, public sidemenu: SidemenuComponent) { }
 
-   toggle:boolean;  
+  toggle: boolean;
 
   ngOnInit(): void {
     //console.log("toggle"+this.toggle);
-    this.employeeservice.getItems();
-    console.log(this.employeeservice.itemList);
+    this.employeeservice.getItems().subscribe(data => {
+      this.itemList = data;
+      this.itemList.Image=btoa(data);
+      console.log(this.itemList);;
+      console.log(this.employeeservice.itemList);
 
+    });
   }
-  testArray=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,171,18,19,20];
 
-  
+
 }
--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Item } from 'src/app/shared/models/item'
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  itemList: Item[]
+  itemList: Item[];
   constructor(private httpClient: HttpClient) { }
 
   // Get all Items 
-  getItems() {
-    this.httpClient.get(environment.apiUrl + '/api/items').toPromise().then(response =>
-      this.itemList = response as Item[]);
+  getItems():Observable<any> {
+    return this.httpClient.get(environment.apiUrl + '/api/items');
   }
 }
