50aa64952b978015ead433ba545ce757f468de11
Aneesh S <aneesh.s@experionglobal.com>
2022-02-04 12:20:50 +0530

    Award Service Added

---
+++ Services/AwardService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using xcart.Models;
+
+namespace xcart.Services
+{
+    public class AwardService : IAwardService
+    {
+        XCartDbContext db;
+        public AwardService(XCartDbContext db)
+        {
+            this.db = db;
+        }
+
+        #region Get All Items
+
+        public async Task<List<Item>> GetAllAwards()
+        {
+            if (db != null)
+            {
+                var list = await db.Item.ToListAsync();
+                return list;
+            }
+            return null;
+        }
+        #endregion
+
+    }
+}
---
+++ Services/IAwardService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using xcart.Models;
+using xcart.ViewModel;
+
+namespace xcart.Services
+{
+    public interface IAwardService
+    {
+        public Task<List<Item>> GetAllAwards();
+    }
+}
