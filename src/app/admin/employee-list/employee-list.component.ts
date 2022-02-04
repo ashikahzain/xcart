05bc960e61bd439e7526107478a893592b90f4b6
Sujith-Sk-work <sujith.kumar@experionglobal.com>
2022-02-04 17:15:10 +0530

    removed overlay

--- src/app/admin/employee-list/employee-list.component.ts
+++ src/app/admin/employee-list/employee-list.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AdminService } from 'src/app/shared/services/admin.service';
-import { Overlay, OverlayRef } from "node_modules/@angular/cdk/overlay";
 import {AddPointComponent} from '../add-point/add-point.component';
-import { ComponentPortal } from "@angular/cdk/portal";
+
 
 @Component({
   selector: 'app-employee-list',
@@ -13,9 +12,8 @@ import { ComponentPortal } from "@angular/cdk/portal";
 export class EmployeeListComponent implements OnInit {
 
   filter:string;
-  overlayRef: OverlayRef;
 
-  constructor(public adminService:AdminService,private router:Router,private overlay: Overlay) { }
+  constructor(public adminService:AdminService,private router:Router) { }
 
   ngOnInit(): void {
     this.adminService.getAllEmployeesPoints();
@@ -27,9 +25,6 @@ export class EmployeeListComponent implements OnInit {
 
 
   open() {
-    this.overlayRef = this.overlay.create();
-    const componentPortal = new ComponentPortal(AddPointComponent);
-    this.overlayRef.addPanelClass("example-overlay");
-    this.overlayRef.attach(componentPortal);
+
   }
 }
