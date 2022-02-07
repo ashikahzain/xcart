c3bb871c53a8162b8b458c1477796d11a64e0d8e
Sujith-Sk-work <sujith.kumar@experionglobal.com>
2022-02-07 10:43:40 +0530

    added modal

--- src/app/admin/employee-list/employee-list.component.ts
+++ src/app/admin/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,FormControl,Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AdminService } from 'src/app/shared/services/admin.service';
 import {AddPointComponent} from '../add-point/add-point.component';
@@ -12,19 +13,59 @@ import {AddPointComponent} from '../add-point/add-point.component';
 export class EmployeeListComponent implements OnInit {
 
   filter:string;
+  addForm:FormGroup;
+  error='';
+  display='none'; //default Variable
 
-  constructor(public adminService:AdminService,private router:Router) { }
+
+  constructor(public adminService:AdminService,private router:Router,private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
     this.adminService.getAllEmployeesPoints();
+
+    //modal
+    //creating form controls and validations
+    this.addForm=this.formBuilder.group({
+      Date:[new Date()],
+      AwardId:['',[Validators.required]],
+      EmployeeId:[''],
+      PresenteeId:[''],
+      Point:[''],
+      Status:['']
+    })
   }
 
   awardHistory(UserId:number){
     this.router.navigate(['admin/awardHistory',UserId]);
   }
 
+    //get controls
+    get formControls(){
+      return this.addForm.controls;
+    }
 
-  open() {
 
-  }
+  // Model Driven Form - login
+    addPoint(){
+      console.log(this.addForm.value);
+
+          //if data is invalid
+    if(this.addForm.invalid){
+      this.error="Invalid Input";
+      return;
+    }
+
+    if(this.addForm.valid){
+      console.log("added Point"+this.addForm.value);
+    }
+
+    }
+
+  openModalDialog(){
+    this.display='block'; //Set block css
+ }
+
+ closeModalDialog(){
+  this.display='none'; //set none css after close dialog
+ }
 }
